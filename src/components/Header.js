236bf5c1ce1d2bd12c58d771f65b544b0d5f4a67
Mariana Lobo <m.lobocasas@gmail.com>
2023-03-14 11:33:59 +0100

    add Header and Dummy components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+function Header(props) {
+    return (
+        <header>
+            <h1 className='header__title'>Juego del ahorcado</h1>
+        </header>
+    );
+}
+
+export default Header;
